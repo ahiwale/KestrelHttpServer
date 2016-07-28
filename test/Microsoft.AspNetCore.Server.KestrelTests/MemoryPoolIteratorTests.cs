c0a7ed4f2ac230778b202544cd34cc13ee963891
Cesar Blum Silveira <cesars@microsoft.com>
2016-07-28 14:17:00 -0700

    Sort usings.

--- test/Microsoft.AspNetCore.Server.KestrelTests/MemoryPoolIteratorTests.cs
+++ test/Microsoft.AspNetCore.Server.KestrelTests/MemoryPoolIteratorTests.cs
@@ -2,12 +2,12 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using System.Linq;
-using Microsoft.AspNetCore.Server.Kestrel.Internal.Infrastructure;
-using System.Numerics;
-using Xunit;
-using System.Text;
 using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using Microsoft.AspNetCore.Server.Kestrel.Internal.Infrastructure;
+using Xunit;
 
 namespace Microsoft.AspNetCore.Server.KestrelTests
 {
